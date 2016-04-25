4bb0bd3c9dd4888715d3b3ba0f77590142f7d7bb
Ryo@Kate <syanidar@gmail.com>
2016-04-26 00:28:40 +0900

    Assimilated the Test class to the newly arranged frame work.

--- Hamster/src/jp/gr/java_conf/syanidar/chess/hamster/Test.java
+++ Hamster/src/jp/gr/java_conf/syanidar/chess/hamster/Test.java
@@ -3,71 +3,76 @@ package jp.gr.java_conf.syanidar.chess.hamster;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
+import jp.gr.java_conf.syanidar.algorithm.mosquito.game.AIPlayer;
+import jp.gr.java_conf.syanidar.algorithm.mosquito.game.Game;
+import jp.gr.java_conf.syanidar.algorithm.mosquito.game.HumanPlayer;
+import jp.gr.java_conf.syanidar.algorithm.mosquito.game.MoveSelector;
+import jp.gr.java_conf.syanidar.algorithm.mosquito.game.NoMoveHandler;
+import jp.gr.java_conf.syanidar.algorithm.mosquito.game.Viewer;
 import jp.gr.java_conf.syanidar.algorithm.mosquito.minimax.AlphaBetaAnalyzer;
 import jp.gr.java_conf.syanidar.algorithm.mosquito.minimax.AlphaBetaSetting;
 import jp.gr.java_conf.syanidar.chess.hamster.game.CentiPawn;
-import jp.gr.java_conf.syanidar.chess.hamster.game.ChessMove;
 import jp.gr.java_conf.syanidar.chess.hamster.game.ChessPosition;
 import jp.gr.java_conf.syanidar.chess.hamster.game.HamsterEvaluator;
-import jp.gr.java_conf.syanidar.chess.hamster.materials.ColorEnum;
-import jp.gr.java_conf.syanidar.utils.collection.ArrayUtility;
-import jp.gr.java_conf.syanidar.utils.collection.ListUtility;
 
 public class Test {
-
-	public static void main(String[] args) throws IOException {
-		AlphaBetaAnalyzer<ChessPosition, CentiPawn> ms = new AlphaBetaAnalyzer<>(HamsterEvaluator.getInstance());
-		ChessPosition cp = new ChessPosition();
-		while(true){
-			System.out.println(cp);
-			List<ChessMove> moveList = cp.moves();
-			if(moveList.isEmpty())break;
-			if (cp.colorToPlay() == ColorEnum.BLACK || cp.colorToPlay() == ColorEnum.WHITE) {
-				ChessMove[] moves = moveList.toArray(new ChessMove[0]);
-				int length = moves.length;
-				CentiPawn[] evaluations = new CentiPawn[length];
-				int bestIndex = -1;
-				List<String> sequence = new ArrayList<>();
-				for (int i = 0; i < length; i++) {
-					List<String> buf = new ArrayList<>();
-					moves[i].play();
-					evaluations[i] = ms.evaluate(cp, new AlphaBetaSetting(2), buf);
-					moves[i].undo();
-					if(bestIndex == -1 || evaluations[i].isBetterThan(evaluations[bestIndex])){
-						bestIndex = i;
-						sequence.clear();
-						sequence.add(moves[i].toString());
-						sequence.addAll(buf);
-					}
-				}		
-				moves[bestIndex].play();
-				ArrayUtility.sort(evaluations, moves);
-				System.out.println(Arrays.asList(moves));
-				System.out.println(Arrays.asList(evaluations));
-				System.out.println(sequence);
-				
-				System.out.println();
-			}else{
-				select(moveList).play();	
-				System.out.println();
+	
+	public static final void main(String...strings){
+		HamsterEvaluator he = HamsterEvaluator.getInstance();
+		AlphaBetaAnalyzer<ChessPosition, CentiPawn> aba = new AlphaBetaAnalyzer<>(he);
+		AlphaBetaSetting<CentiPawn> abs = new AlphaBetaSetting<>(3, he);
+		AIPlayer<ChessPosition, CentiPawn, AlphaBetaSetting<CentiPawn>> ap = new AIPlayer<>(aba, abs, he);
+		HumanPlayer<ChessPosition> hp = new HumanPlayer<>(new ChessMoveSelector());
+		ChessPosition position = new ChessPosition();
+		Game<ChessPosition> game = new Game<>(position, ap, ap, new ChessViewer());
+		ChessNoMoveHandler ch = new ChessNoMoveHandler();
+		
+		while(!ch.terminated){
+			game.play(ch);
+			if(position.theFirstPlayerHasTheMove()){
+				System.out.println(ap.evaluationMap());
 			}
 		}
 	}
-	
-	private static final ChessMove select(List<ChessMove> moves) throws IOException{
-		System.out.println(Arrays.asList(moves));
-		System.out.print("Select the move > ");
-		List<String> notations = ListUtility.map(moves, m -> m.toString());
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String input = br.readLine();
-		for(int i = 0, n = moves.size(); i < n; i++){
-			if(input.equals(notations.get(i)))return moves.get(i);
+	private static class ChessNoMoveHandler implements NoMoveHandler<ChessPosition>{
+		private boolean terminated;
+		@Override
+		public void handle(ChessPosition position) {
+			if(position.isInCheck()){
+				System.out.println(position.colorToPlay().opposite() + " WINS THE GAME.");
+			}
+			terminated = true;
 		}
-		System.out.println("Invalid");
-		return select(moves);
+	}
+	private static class ChessMoveSelector implements MoveSelector{
+		@Override
+		public String select(List<String> moves) {
+			BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+			
+			System.out.println(moves);
+			System.out.print("Select the move >");
+			
+			String input = null;
+			try {
+				input = reader.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+				throw new RuntimeException();
+			}
+			return input;
+		}
+	}
+	private static class ChessViewer implements Viewer<ChessPosition>{
+		@Override
+		public void drawBoard(ChessPosition position) {
+			System.out.println(position);
+		}
+		@Override
+		public void drawComputedLine(Optional<List<String>> line) {
+			line.ifPresent(System.out::println);
+		}
 	}
 }
