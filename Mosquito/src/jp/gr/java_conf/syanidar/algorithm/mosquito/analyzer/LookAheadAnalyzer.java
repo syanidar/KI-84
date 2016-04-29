49a58e09b33f30a1f800bd4407adc98c52c94c19
Ryo@Chiaki <syanidar@gmail.com>
2016-04-29 17:51:26 +0900

    Deleted the obsolete interface

--- Mosquito/src/jp/gr/java_conf/syanidar/algorithm/mosquito/analyzer/LookAheadAnalyzer.java
+++
@@ -1,7 +0,0 @@
-package jp.gr.java_conf.syanidar.algorithm.mosquito.analyzer;
-
-import java.util.List;
-
-public interface LookAheadAnalyzer<P extends Position<?>, S extends Setting, E extends Evaluation<?>> extends Analyzer<P, S, E> {
-	public E evaluate(P position, S setting, List<String> line);
-}
